999edfd18ae5489cf21aeee86d521b32b279e99e
me-x-mi <developerezrqn@gmail.com>
2019-06-24 23:54:52 +0300

    generate unique phone number

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      numbersGenerated: [1, 2, 3],
+      generatedPhoneNumber: ''
+    }
+  }
+
+  // Generate new phone number function
+  generateNewPhoneNumber = () => {
+    // Generate 9 random numbers
+    const { numbersGenerated } = this.state;
+    const number = `0${Math.floor(Math.random() * 1000000000)}`;
+
+    // Check whether the number already exist
+    if (numbersGenerated.includes(number)) {
+      console.log('---->>>>>Please genarte a new number')
+    } else {
+      numbersGenerated.push(number);
+      // Set the current generated numbers(Add 0 at the beginning)
+      this.setState({
+        generatedPhoneNumber: number
+      });
+    }
+  }
+
+  render() {
+    const { generatedPhoneNumber } = this.state;
+    return (
+      <div className="App">
+        <h3>New Generator APP</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to new phone number generator app.
+          Genrate and store your phone numbers for retrival
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div>
+          <button onClick={() => this.generateNewPhoneNumber()}>Generate</button>
+        </div>
+        <p>Current Phone Number: <b>{generatedPhoneNumber}</b></p>
+        <h3>Check New Phone Number</h3>
+        <div className="Download-pdf-file">
+          <button>Download As PDF</button>
+        </div>
+        <div className="Phone-Number-table">
+          <table>
+            <thead>
+              <tr>
+                <th>Phone Numbers</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>07893637838</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('APP', () => {
+  it('expect 1+1 to equal 2', () => {
+    expect(1 + 1).toEqual(2)
+  });
+
+  it('renders without crashing', () => {
+    shallow(<App />);
+  })
+})
