b6dc937823d25d25367a5a586caa37b12ea4a7d2
me-x-mi <ezrqnkemboi@gmail.com>
2019-07-14 01:08:46 +0300

    do css and designs for the whole page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      numbersGenerated: [1, 2, 3],
+      numbersGenerated: [],
       generatedPhoneNumber: '',
       amount: 0
     }
@@ -26,55 +26,128 @@ class App extends React.Component {
   // Generate new phone number function
   generateNewPhoneNumber = () => {
     // Generate 9 random numbers
-    const { numbersGenerated, amount } = this.state;
-    // for(i = 0; i <= amount; i++) {
-    //   const number = `0${Math.floor(Math.random() * 1000000000)}`;
-    // }
     let number;
-    number = `0${Math.floor(Math.random() * 1000000000)}`;
-    // Check whether the number already exist
-    if (!numbersGenerated.includes(number)) {
-      numbersGenerated.push(number);
-      // Set the current generated numbers(Add 0 at the beginning)
-      this.setState({
-        generatedPhoneNumber: number
-      });
+    const { numbersGenerated, amount } = this.state;
+    for (let i = 0; i <= amount; i++) {
+      number = `0${Math.floor(Math.random() * 1000000000)}`;
+      // Check already created numbers
+      if (!numbersGenerated.includes(number)) {
+        numbersGenerated.push(number);
+        // Set the current generated numbers(Add 0 at the beginning)
+        this.setState({
+          generatedPhoneNumber: number
+        });
+      }
     }
   }
 
   render() {
     const { generatedPhoneNumber, amount } = this.state;
     return (
-      <div className="App">
-        <h3>New Generator APP</h3>
-        <p>
-          Welcome to new phone number generator app.
-          Genrate and store your phone numbers for retrival
-        </p>
-        <div>
-          <form >
-            <input name="phonenumber" name={amount} />
-            <button id="generate-numbers" onClick={() => this.generateNewPhoneNumber()}>Generate</button>
-          </form>
-        </div>
-        <p>Current Phone Number: <b>{generatedPhoneNumber}</b></p>
-        <h3>Check New Phone Number</h3>
-        <div className="Download-pdf-file">
-          <button>Download As PDF</button>
-        </div>
-        <div className="Phone-Number-table">
+      <div>
+        <h3 id="app-title">PhoneGen - Phone Generator App</h3>
+        <div className="App">
+          <div className="App__left">
+            <h3>PhoneGen</h3>
+            <p id="app-description">
+              Welcome to new phone number generator app.
+              Generate and store your phone numbers for retrival
+            </p>
+            <input name="phone" placeholder="Generate 1 to 10000" />
+            <button
+              id="generate-numbers"
+              onClick={() => this.generateNewPhoneNumber()}
+            >
+              Generate
+            </button>
+          </div>
+          <div className="App__right">
+            <div className="download-pdf-file">
+              <button>Download As Pdf</button>
+            </div>
+            <div className="headers-for-phones">
+              <div className="headers-for-phones__index">
+                <h3>Index</h3>
+              </div>
+              <div className="headers-for-phones__number">
+                <h3>Phone Number</h3>
+              </div>
+            </div>
+            <div className="phone-list-table">
+              <div className="phone-list-table__left">
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+              </div>
+              <div className="phone-list-table__right">
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+                <p>072509354</p>
+              </div>
+              {/* <table>
+                <thead>
+                  <tr>
+                    <td>Index</td>
+                    <td>Phone Numbers</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>{generatedPhoneNumber}</td>
+                  </tr>
+                </tbody>
+              </table> */}
+            </div>
+
+            {/* <div className="Download-pdf-file">
+            <button>Download As PDF</button>
+          </div>
           <table>
             <thead>
               <tr>
-                <th>Phone Numbers</th>
+                <th>Index</th>
+                <th>Phone Number</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>07893637838</td>
+                <td>1</td>
+                <td>{generatedPhoneNumber}</td>
               </tr>
             </tbody>
-          </table>
+          </table> */}
+          </div>
         </div>
       </div>
     )
